a6b99d8ca9e41f3ef7c8ff5e8a88b64d90acfcdf
Worratep1 <Worratepputed@gmail.com>
2024-11-25 15:29:44 +0700

    worratep

---
+++ question2-3.dart
@@ -0,0 +1,9 @@
+void main() {
+  int i = 1; 
+
+  while (i <= 12) { 
+    int sum = 5 * i;
+    print("5 x $i = $sum");
+    i++; 
+  }
+}
---
+++ question2-4.dart
@@ -0,0 +1,10 @@
+void main() {
+ for(int num=1; num<=9;num++){
+  print('เเม่ $num');
+  for(int i=1; i<=12; i++){
+    int sum= num*i;
+    print("$num x $i = $sum");
+  }
+  print('');
+ }
+}
