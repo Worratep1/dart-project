a21ed0a0e8f17c47e0badb6bf81f89e28659b86f
Worratep1 <Worratepputed@gmail.com>
2024-11-25 16:30:30 +0700

    ข้อความช่วยจำว่าแก้ไขอะไรไป

--- question2-3.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  int i = 1; 
-
-  while (i <= 12) { 
-    int sum = 5 * i;
-    print("5 x $i = $sum");
-    i++; 
-  }
-}
--- question3-2.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-bool isEven (int number){
-   return number % 2 ==0;
-} 
-void main(){
- print('Enter the number:');
-  int number = int.parse(stdin.readLineSync()!);
-  print(isEven(number));
-}
